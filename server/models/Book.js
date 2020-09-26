c74d374e827ea07a5ffe724f586e738038545f49
mhans19 <mdhansen19@gmail.com>
2020-09-26 15:20:25 -0600

    Reviewed and Annotated Models

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,5 +1,5 @@
+// DEPENDENCIES
 const { Schema } = require('mongoose');
-
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
   authors: [
@@ -27,5 +27,5 @@ const bookSchema = new Schema({
     required: true,
   },
 });
-
+// EXPORTS
 module.exports = bookSchema;
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,8 @@
+// DEPENDENCIES
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
-
-// import schema from Book.js
-const bookSchema = require('./Book');
-
+const bookSchema = require('./Book'); // IMPORT Schema from Book.js
+// DEFINE User Schema
 const userSchema = new Schema(
   {
     username: {
@@ -21,37 +20,33 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
+    // Set savedBooks as array of data that adheres to bookSchema
     savedBooks: [bookSchema],
   },
-  // set this to use virtual below
+  // Set this to use virtual below
   {
     toJSON: {
       virtuals: true,
     },
   }
 );
-
-// hash user password
+// HASH user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
-
   next();
 });
-
-// custom method to compare and validate password for logging in
+// Custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
-
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+// When we query a user, we'll also get another field called `bookCount` with the number of saved books we have
 userSchema.virtual('bookCount').get(function () {
   return this.savedBooks.length;
 });
-
+// DEFINE User
 const User = model('User', userSchema);
-
+// EXPORTS
 module.exports = User;
