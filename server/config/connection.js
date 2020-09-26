463da2de0deb35b43edcab79cf2e8c8db7eb7216
mhans19 <mdhansen19@gmail.com>
2020-09-26 15:24:43 -0600

    Reviewed and Annotated mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
+// DEPENDENCIES
 const mongoose = require('mongoose');
-
+// MONGOOSE CONNECTION
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: true,
 });
-
+// EXORTS
 module.exports = mongoose.connection;
