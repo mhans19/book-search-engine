c36a32354ac801ebbe5e954518fda42c5b5e7ed8
mhans19 <mdhansen19@gmail.com>
2020-09-26 14:34:33 -0600

    updated authentication

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,5 @@
+// DEPENDENCIES
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+const { User } = require("../models");
+// MUTATIONS
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,11 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
   authMiddleware: function (req) {
     // allows token to be sent via  req.query or headers
@@ -30,10 +35,5 @@ module.exports = {
 
     // send to next endpoint
     return req;
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
