f23413b40d5a17e47ac8b67e18f0d14e0da6fc4d
mhans19 <mdhansen19@gmail.com>
2020-09-27 11:04:50 -0600

    tested connection with Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,9 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
 // DEFINE CONNECTION
-const MONGODB_URI = `mongodb+srv://mhans19:${process.env.DB_PASS}@cluster0.bhbgx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&readPreference=primary`
+//console.log(process.env.DB_NAME);
+const MONGODB_URI = `mongodb+srv://mhans19:${process.env.DB_PASS}@cluster0.bhbgx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&replicaSet=primary`
+
 // MONGOOSE CONNECTION
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
