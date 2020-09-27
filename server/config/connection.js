32a451567cce3d33f741007844be1f69cddf0ca1
mhans19 <mdhansen19@gmail.com>
2020-09-27 10:07:14 -0600

    Adjusted Mongo URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
 // DEFINE CONNECTION
-const MONGODB_URI = `mongodb+srv://mhans19:${process.env.DB_PASS}@cluster0.bhbgx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const MONGODB_URI = `mongodb+srv://mhans19:${process.env.DB_PASS}@cluster0.bhbgx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&readPreference=primary`
 // MONGOOSE CONNECTION
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
