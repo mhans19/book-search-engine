db51881b593b87e2e5423176c63b5a0c866babdf
mhans19 <mdhansen19@gmail.com>
2020-09-27 11:14:31 -0600

    Another Heroku attempt

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv").config();
 // DEFINE CONNECTION
 //console.log(process.env.DB_NAME);
 const MONGODB_URI = `mongodb+srv://mhans19:${process.env.DB_PASS}@cluster0.bhbgx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&replicaSet=primary`
-
 // MONGOOSE CONNECTION
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
